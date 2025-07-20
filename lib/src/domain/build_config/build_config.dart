9c8d7dea1267085105a436e861e7989aba2ca0f1
Yaroslav Halivets <yarikgalivetc@gmail.com>
2025-07-20 23:24:21 +0200

    [MS-18] fix poster image receiving

--- lib/src/domain/build_config/build_config.dart
+++ lib/src/domain/build_config/build_config.dart
@@ -20,7 +20,7 @@ final class BuildConfig {
       enableLogs: true,
       apiKey: _apiKey,
       baseUrl: 'https://api.themoviedb.org/3',
-      baseImageUrl: 'https://image.tmdb.org/t/p/',
+      baseImageUrl: 'https://image.tmdb.org/t/p/w500',
     );
   }
 
