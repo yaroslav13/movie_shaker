713710adee3e8cf124740500a8e2b985709255ed
Yaroslav Halivets <yarikgalivetc@gmail.com>
2026-02-15 11:58:08 +0100

    [MS-1020] use Throws out of DCM annotations

--- lib/src/domain/repositories/movie_genres_repository.dart
+++ lib/src/domain/repositories/movie_genres_repository.dart
@@ -1,8 +1,8 @@
-import 'package:movie_shaker/src/domain/annotations/throws.dart';
+import 'package:dart_code_metrics_annotations/annotations.dart';
 import 'package:movie_shaker/src/domain/entities/genre/genre.dart';
 import 'package:movie_shaker/src/domain/exceptions/network_exception.dart';
 
 abstract interface class MovieGenresRepository {
-  @Throws([NetworkException])
+  @Throws({NetworkException})
   Future<List<Genre>> getMovieGenres();
 }
